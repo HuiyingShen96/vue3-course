e89534262d6325a1ebf43af330d2fdc174f4360c
HuiyingShen96 <18218343475@163.com>
2024-12-18 15:38:01 +0800

    chapter11 布局组件之栅格化

--- chapter/08/packages/components/example.vue
+++ chapter/08/packages/components/example.vue
@@ -5,10 +5,62 @@
     <Button @click="showMessage">Message</Button>
     <Button @click="showDialog">Dialog</Button>
   </Box>
+  <Row>
+    <Col class="gird" :span="24">col-24</Col>
+  </Row>
+  <Row>
+    <Col class="gird" :span="12">col-12</Col>
+    <Col class="gird gird-dark" :span="12">col-12</Col>
+  </Row>
+  <Row>
+    <Col class="gird" :span="8">col-8</Col>
+    <Col class="gird gird-dark" :span="8">col-8</Col>
+    <Col class="gird" :span="8">col-8</Col>
+  </Row>
+  <Row>
+    <Col class="gird" :span="6">col-6</Col>
+    <Col class="gird gird-dark" :span="6">col-6</Col>
+    <Col class="gird" :span="6">col-6</Col>
+    <Col class="gird gird-dark" :span="6">col-6</Col>
+  </Row>
+  <Row>
+    <Col class="gird" :span="4">col-4</Col>
+    <Col class="gird gird-dark" :span="4">col-4</Col>
+    <Col class="gird" :span="4">col-4</Col>
+    <Col class="gird gird-dark" :span="4">col-4</Col>
+    <Col class="gird" :span="4">col-4</Col>
+    <Col class="gird gird-dark" :span="4">col-4</Col>
+  </Row>
+  <Row>
+    <Col class="gird" :span="3">col-3</Col>
+    <Col class="gird gird-dark" :span="3">col-3</Col>
+    <Col class="gird" :span="3">col-3</Col>
+    <Col class="gird gird-dark" :span="3">col-3</Col>
+    <Col class="gird" :span="3">col-3</Col>
+    <Col class="gird gird-dark" :span="3">col-3</Col>
+    <Col class="gird" :span="3">col-3</Col>
+    <Col class="gird gird-dark" :span="3">col-3</Col>
+  </Row>
+  <Row>
+    <Col class="gird" :span="2">col-2</Col>
+    <Col class="gird gird-dark" :span="2">col-2</Col>
+    <Col class="gird" :span="2">col-2</Col>
+    <Col class="gird gird-dark" :span="2">col-2</Col>
+    <Col class="gird" :span="2">col-2</Col>
+    <Col class="gird gird-dark" :span="2">col-2</Col>
+    <Col class="gird" :span="2">col-2</Col>
+    <Col class="gird gird-dark" :span="2">col-2</Col>
+    <Col class="gird" :span="2">col-2</Col>
+    <Col class="gird gird-dark" :span="2">col-2</Col>
+    <Col class="gird" :span="2">col-2</Col>
+    <Col class="gird gird-dark" :span="2">col-2</Col>
+  </Row>
 </template>
 
 <script setup lang="ts">
-import { Button, Box, Message, Dialog } from './src';
+import { Button, Box, Message, Dialog,
+  Col, Row,
+ } from './src';
 import { prefixName } from './src/theme/index';
 
 const onClick = () => {
@@ -39,9 +91,29 @@ const showDialog = () => {
 
 <style>
 .example {
-  height: 100%;
+  /* height: 100%; */
   padding: 100px;
   box-sizing: border-box;
   text-align: center;
+}
+</style>
+
+<style lang="less">
+.gird {
+  background: #0092ff6b;
+  border: 1px #0092ff6b solid;
+  margin-bottom: 10px;
+  height: 50px;
+  color: #035593;
+  font-size: 18px;
+  // font-weight: bolder;
+  text-align: center;
+  line-height: 50px;
+  &.gird-dark {
+    background: #068aed;
+    border: 1px #068aed solid;
+    color: #ffffff;
+    margin-bottom: 10px;
+  }
 }
 </style>
---
+++ chapter/08/packages/components/src/col/src/col.vue
@@ -0,0 +1,29 @@
+<template>
+  <div :class="{
+    [baseClassName]: true,
+    [spanClassName]: true,
+  }">
+    <slot v-if="$slots.default"></slot>
+  </div>
+</template>
+
+<script lang="ts" setup>
+import { prefixName } from '../../theme';
+
+const props = defineProps({
+  span: Number
+});
+
+const getSpan = (propSpan: number|undefined): number => {
+  if (typeof propSpan === 'number') {
+    const span = Math.ceil(Number(propSpan));
+    if (span >= 1 && span <= 24) {
+      return span;
+    }
+  }
+  return 1;
+};
+
+const baseClassName = `${prefixName}-col`;
+const spanClassName = `${prefixName}-col-${getSpan(props.span)}`;
+</script>
